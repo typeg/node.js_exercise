6763961ebe210d98e0e316af52036f7626739a60
Jack CH Lee <changhyun.lee@gmail.com>
2016-02-26 14:58:08 +0900

    streaming server test

--- helloweb.js
+++ helloweb.js
@@ -1,10 +1,15 @@
 // helloweb.js
-
+// 2016-02-26 modified to see streaming
+//
 var http = require("http");
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {"Content-Type":"text/plain"});
-	res.write("Hello\r\n");
-	res.write("World\r\n");
-	res.end();
+	res.write("He");
+	res.write("llo\r\n");
+
+	setTimeout(function(){
+		res.write("World\r\n");
+		res.end();
+	}, 3000);
 }).listen(8080);
