8f473b478872b49e3bdda49ff1f8847e8b09760e
Jack CH Lee <changhyun.lee@gmail.com>
2016-02-26 15:26:45 +0900

    change ls param

--- showls.js
+++ showls.js
@@ -1,9 +1,10 @@
 // showls.js
+// 2016-02-26 replace from sys to util
 
 var	sys = require("sys"),
 	spawn = require("child_process").spawn;
 
-var	ls = spawn("ls", ["-ls", "/"]);
+var	ls = spawn("ls", ["-la", "/"]);
 ls.stdout.addListener("data", function(data){
-	sys.print(data);
+	sys.print( data);
 });
