e4bcc8b9bd07615530273e842535c5c68e66da95
Jack CH Lee <changhyun.lee@gmail.com>
2016-02-26 14:48:34 +0900

    web server open test

---
+++ helloweb.js
@@ -0,0 +1,10 @@
+// helloweb.js
+
+var http = require("http");
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {"Content-Type":"text/plain"});
+	res.write("Hello\r\n");
+	res.write("World\r\n");
+	res.end();
+}).listen(8080);
