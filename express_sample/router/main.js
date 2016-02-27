f6ef1d24d76af6b2f1db7c2ef39a8a23d819f7bc
Jack CH Lee <changhyun.lee@gmail.com>
2016-02-28 01:21:56 +0900

    ...

--- express_sample/router/main.js
+++ express_sample/router/main.js
@@ -1,10 +1,73 @@
-module.exports = function(app)
+module.exports = function(app, fs)
 {
 	app.get('/', function(req, res){
-		res.render('index.html')
+		var sess = req.session;
+
+		res.render('index', {
+			title: "myhome",
+			length: 5,
+			name: sess.name,
+			username: sess.username
+		})
 	});
 	app.get('/about', function(req, res){
 		res.render('about.html');
 	});
-	
+
+	app.get('/list', function(req,res){
+		fs.readFile(__dirname + "/../data/"+"user.json", 'utf8', function(err,data){
+			console.log(data);
+			res.end(data);
+		});
+	});
+	app.get('/getUser/:username', function(req,res){
+		fs.readFile(__dirname + "/../data/"+"user.json", 'utf8', function(err,data){
+			var users = JSON.parse(data);
+			res.json(users[req.params.username]);
+		});
+	});
+
+	app.get('/login/:username/:password', function(req,res){
+		var sess;
+		sess = req.session;
+		
+		fs.readFile(__dirname+"/../data/user.json", "utf8", function(err,data){
+			var users = JSON.parse(data);
+			var username = req.params.username;
+			var password = req.params.password;
+			var result = {};
+			if(!users[username]){
+				result["success"]=0;
+				result["error"]="user not found";
+//				res.json(result);
+				return;
+			}
+			if(users[username]["password"]==password){
+				result["success"] = 1;
+				sess.username = username;
+				sess.name = users[username]["name"];
+//				res.json(result);
+			}else{
+				result["success"]=0;
+				result["error"]="incorrect password";
+//				res.json(result);
+			}
+			res.redirect('/');
+		})
+	});
+
+        app.get('/logout', function(req, res){
+        	sess = req.session;
+        	if(sess.username){
+            	req.session.destroy(function(err){
+                	if(err){
+                    		console.log(err);
+                	}else{
+                    		res.redirect('/');
+                	}
+            	})
+        	}else{
+            		res.redirect('/');
+        	}
+    	});
 }
--- express_sample/server.js
+++ express_sample/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
@@ -10,3 +12,11 @@ var server = app.listen(8888, function(){
 	console.log("Express server has started on port 8888")
 });
 app.use(express.static('public'));
+app.use(cookieParser());
+app.use(session({
+	secret: '!@#mykey#@!',
+	resave: false,
+	saveUninitialized: true
+}));
+
+var router = require('./router/main')(app, fs);
