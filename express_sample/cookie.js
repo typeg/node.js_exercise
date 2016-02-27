691785dec7ef05573089f3f0d723a389aab912eb
Jack CH Lee <changhyun.lee@gmail.com>
2016-02-27 15:20:54 +0900

    show cookie info on console

--- express_sample/cookie.js
+++ express_sample/cookie.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express()
 	.use(function(req,res){
+		console.log('---client request cookies header:\n', req.headers['cookie']);
 		res.cookie('name', 'naeyo');
 		res.end('Hello!');
 	})
